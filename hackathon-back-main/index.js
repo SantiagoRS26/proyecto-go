1705d94143abd06edb10c2e3c362ec903dca1ab4
SantiagoRS26 <santiagors2611@gmail.com>
2024-11-24 13:23:22 -0500

    Update index.js CORS

--- hackathon-back-main/index.js
+++ hackathon-back-main/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const sessionRoutes = require('./routes/sessionRoutes');
-const reportRoutes = require('./routes/reports/reportRoutes')
+const reportRoutes = require('./routes/reports/reportRoutes');
 const userGeofencesRoutes = require('./routes/usersGeofences/userGeofencesRoutes');
 const notifyRoutes = require('./routes/notifications/notificationsRoutes');
 const trafficRestrictionRoutes = require('./routes/traffic-restrictions/trafficRoutes');
@@ -10,29 +10,53 @@ const interestZoneRoutes = require('./routes/interestZone/interestZoneRoutes');
 const newsRoutes = require('./routes/generalImportantNews/newsRouter');
 const chatbotRoutes = require('./routes/chatbot/chatbotRoutes');
 const dotenv = require('dotenv');
-const cors =  require('cors')
-
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
 
+const allowedOrigins = [
+    'https://proyecto-go-jubc-mx40m5kc3-santiagors26s-projects.vercel.app', // Dominio del frontend
+    'http://localhost:3000', // Dominio para desarrollo local
+];
+
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        credentials: true,
+    })
+);
+
+// Manejar solicitudes preflight
+app.options('*', cors());
+
+// Conexión a la base de datos
 connectDB();
 
+// Rutas
 app.use(sessionRoutes);
 app.use(reportRoutes);
-app.use(userGeofencesRoutes)
+app.use(userGeofencesRoutes);
 app.use(notifyRoutes);
 app.use(trafficRestrictionRoutes);
 app.use(interestZoneRoutes);
 app.use(newsRoutes);
 app.use(chatbotRoutes);
-app.get("/", (req, res)=>{
-    res.send("Welcome to the API");
-})
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the API');
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+app.listen(port, () =>
+    console.log(`Server running on ${port}, http://localhost:${port}`)
+);
